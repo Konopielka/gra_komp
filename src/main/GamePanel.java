ef1f38008b785bb6482295d28fd7fe91fa69547d
Kono <szwaj.jakub@gmail.com>
2022-08-03 17:56:10 +0200

    foka png added

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,7 +10,7 @@ public class GamePanel extends JPanel implements Runnable{
     //SCREEN SETTINGS
     final int originalTileSize = 16; //16x16 tile
     final int scale = 3;
-    final int tileSize = originalTileSize * scale; //48x48 tile
+    public final int tileSize = originalTileSize * scale; //48x48 tile
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; //768 pxs
@@ -17,6 +19,7 @@ public class GamePanel extends JPanel implements Runnable{
     int FPS = 60;
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
     //set player default position
     int playerX = 100;
@@ -40,47 +43,43 @@ public class GamePanel extends JPanel implements Runnable{
     public void run() {
 
         double drawInterval = 1000000000/FPS;
-        double nextDrawTime = System.nanoTime() + drawInterval;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
 
         //gameLoop
         while(gameThread != null){
             //System.out.println("The game loop is runnig");
 
-
-
             //1 update info of character position
-            update();
+           // update();
             //2 draw screen with updated info
-            repaint();
+          //  repaint();
 
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime)/ drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
 
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime = remainingTime/1000000;
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
 
-                if (remainingTime < 0){
-                    remainingTime = 0;
-                }
-                Thread.sleep((long) remainingTime);
-
-                nextDrawTime += drawInterval;
-
-            }catch (InterruptedException e){
-                e.printStackTrace();
+            if (timer >= 1000000000){
+                System.out.println("FPS: "+ drawCount);
+                drawCount = 0;
+                timer = 0;
             }
         }
     }
     public void update(){
 
-        if(keyH.upPressed == true){
-            playerY -= playerSpeed;
-        }else if(keyH.downPressed == true){
-            playerY += playerSpeed;
-        }else if(keyH.leftPressed == true){
-            playerX -= playerSpeed;
-        }else if (keyH.rightPressed == true){
-            playerX += playerSpeed;
-        }
+        player.update();
     }
     public void paintComponent(Graphics g){
 
@@ -88,8 +87,9 @@ public class GamePanel extends JPanel implements Runnable{
 
         Graphics2D g2 = (Graphics2D)g;
 
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+      /*  g2.setColor(Color.white);
+       g2.fillRect(playerX, playerY, tileSize, tileSize);*/
+        player.draw(g2);
         g2.dispose();
     }
 }
--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -5,7 +5,9 @@ import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
 
-    public boolean upPressed, downPressed, leftPressed, rightPressed;
+    public boolean upPressed, downPressed, leftPressed, rightPressed,
+            upLeftPressed;
+
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -28,6 +30,10 @@ public class KeyHandler implements KeyListener {
         if(code == KeyEvent.VK_D){
             rightPressed = true;
         }
+        if(code == KeyEvent.VK_W && code == KeyEvent.VK_A){
+            upLeftPressed = true;
+
+        }
 
     }
 
@@ -47,6 +53,9 @@ public class KeyHandler implements KeyListener {
         }
         if(code == KeyEvent.VK_D){
             rightPressed = false;
+        }
+        if(code == KeyEvent.VK_W && code == KeyEvent.VK_A) {
+            upLeftPressed = false;
         }
     }
 }
